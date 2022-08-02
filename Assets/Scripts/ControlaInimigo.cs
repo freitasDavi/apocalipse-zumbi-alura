494a3417d03215d24e017394d512eb0be33bb846
Davi Freitas <davi.frrs@outlook.com>
2022-08-01 23:49:52 -0300

    Initial Commit + Class 4

---
+++ Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour
+{
+    public GameObject Jogador;
+    public float Velocidade = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
+
+        Vector3 direcao = Jogador.transform.position - transform.position;
+
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+        if (distancia > 2.5)
+        {
+            GetComponent<Rigidbody>().MovePosition(
+                GetComponent<Rigidbody>().position + (direcao.normalized * Velocidade * Time.deltaTime)
+            );
+
+            GetComponent<Animator>().SetBool("Atacando", false);
+        } else
+        {
+            GetComponent<Animator>().SetBool("Atacando", true);
+        }
+    }
+
+    void AtacaJogador ()
+    {
+        Time.timeScale = 0;
+        Jogador.GetComponent<ControlaJogador>().TextoGameOver.SetActive(true);
+        Jogador.GetComponent<ControlaJogador>().Vivo = false;
+    }
+}
---
+++ Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public float velocidade = 10;
+    Vector3 direcao;
+    public LayerMask MascaraChao;
+    public GameObject TextoGameOver;
+    public bool Vivo = true;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+        
+        direcao = new Vector3()
+        {
+            x = eixoX,
+            z = eixoZ,
+            y = 0
+        };
+
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true); 
+        } else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+
+        if (Vivo == false)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                SceneManager.LoadScene("game");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // É necessário multiplicar pelo tempo, caso contrário andará de acordo com a quantidade de frames por Segundo
+        GetComponent<Rigidbody>().MovePosition(
+                GetComponent<Rigidbody>().position + (direcao * velocidade * Time.deltaTime));
+
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(raio.origin, raio.direction * 100 , Color.red);
+
+        RaycastHit impacto;
+
+        if (Physics.Raycast(raio, out impacto, 100, MascaraChao))
+        {
+            Vector3 posicaoMiraJogador = impacto.point - transform.position;
+
+            posicaoMiraJogador.y = transform.position.y;
+
+            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
+
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
+    }
+}
