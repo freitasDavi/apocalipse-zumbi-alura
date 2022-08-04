070a589f64a4af9e87c8e591ea43bb8ccde67161
Davi Freitas <davi.frrs@outlook.com>
2022-08-04 12:54:26 -0300

    lifebar

---
+++ Assets/Scripts/ControlaInterface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlaInterface : MonoBehaviour
+{
+    private ControlaJogador scriptControlaJogador;
+    public Slider SliderVidaJogador;
+    void Start()
+    {
+        scriptControlaJogador = GameObject.FindWithTag("Jogador")
+                                            .GetComponent<ControlaJogador>();
+
+        SliderVidaJogador.maxValue = scriptControlaJogador.Vida;
+        AtualizarSliderVidaJogador();
+    }
+
+    void Update()
+    {
+    }
+
+    public void AtualizarSliderVidaJogador ()
+    {
+        SliderVidaJogador.value = scriptControlaJogador.Vida;
+    }
+}
--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -9,9 +9,10 @@ public class ControlaJogador : MonoBehaviour
     private Vector3 direcao;
     public LayerMask MascaraChao;
     public GameObject TextoGameOver;
-    public bool Vivo = true;
     private Rigidbody rigidbodyJogador;
     private Animator animatorJogador;
+    public int Vida = 100;
+    public ControlaInterface scriptControlaInterface;
     // Update is called once per frame
 
     private void Start()
@@ -40,7 +41,7 @@ public class ControlaJogador : MonoBehaviour
             animatorJogador.SetBool("Movendo", false);
         }
 
-        if (Vivo == false)
+        if (Vida <= 0)
         {
             if (Input.GetButton("Fire1"))
             {
@@ -69,6 +70,19 @@ public class ControlaJogador : MonoBehaviour
             Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
 
             rigidbodyJogador.MoveRotation(novaRotacao);
+        }
+    }
+
+    public void TomarDano (int dano)
+    {
+        Vida -= dano;
+
+        scriptControlaInterface.AtualizarSliderVidaJogador();
+
+        if (Vida <= 0)
+        {
+            Time.timeScale = 0;
+            TextoGameOver.SetActive(true);
         }
     }
 }
